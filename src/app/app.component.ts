16bc5efbed95f2048af3416876b713bac839480a
Alessandro F. Castelli <alessandrofrancescocastelli@outlook.it>
2024-11-20 17:54:49 +0100

    Hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ViewComponent} from "./components/view/view.component";
 import {NavbarComponent} from "./components/navbar/navbar.component";
+import {HeroSectionComponent} from "./components/hero-section/hero-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, ViewComponent, NavbarComponent],
+    imports: [RouterOutlet, ViewComponent, NavbarComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
