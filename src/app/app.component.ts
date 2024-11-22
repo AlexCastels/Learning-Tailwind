fca52987494f393e41f13515d039be1b8a8a2353
Alessandro F. Castelli <alessandrofrancescocastelli@outlook.it>
2024-11-22 14:06:04 +0100

    Updated View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, HostListener, OnChanges, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {ViewComponent} from "./components/view/view.component";
 import {NavbarComponent} from "./components/navbar/navbar.component";
 import {HeroSectionComponent} from "./components/hero-section/hero-section.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
+    selector: 'app-root',
+    standalone: true,
     imports: [RouterOutlet, ViewComponent, NavbarComponent, HeroSectionComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'newProj';
+    // @HostListener('window:scroll', ['$event'])
+    //
+    // onScroll(event: Event): void {
+    //     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    //     console.log('Window Scroll position:', scrollPosition);
+    //     console.log(event)
+    // }
 }
