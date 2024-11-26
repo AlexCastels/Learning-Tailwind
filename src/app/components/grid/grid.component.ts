fd5852cfcd175d5b7d5a68761f24e10a7755679b
Alessandro F. Castelli <alessandrofrancescocastelli@outlook.it>
2024-11-26 14:33:29 +0100

    Grid Styling

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,12 +1,24 @@
 import {Component} from '@angular/core';
+import {NgForOf} from "@angular/common";
 
 @Component({
     selector: 'app-grid',
     standalone: true,
-    imports: [],
+    imports: [
+        NgForOf
+    ],
     templateUrl: './grid.component.html',
     styleUrl: './grid.component.css'
 })
 export class GridComponent {
-
+    items = [
+        {box : 1},
+        {box : 2},
+        {box : 3},
+        {box : 4},
+        {box : 5},
+        {box : 6},
+        {box : 7},
+        {box : 8},
+    ]
 }
